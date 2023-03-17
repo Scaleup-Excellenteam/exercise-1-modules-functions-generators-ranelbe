8600ac38dff06f946e389874d41a467187c181ab
ranelbe <ranelbe@edu.hac.ac.il>
2023-03-17 12:28:20 +0200

    ex6 - connected_vessels

---
+++ 5.4.connected_vessels.py
@@ -0,0 +1,34 @@
+"""
+Writer: Ranel Ben Simman Tov, 315967703
+"""
+from itertools import zip_longest
+
+
+def interleave(*iterables):
+    """
+    Interleave the given iterables - regular implementation
+    :param iterables: the iterables to interleave
+    :return: an interleaved list of all the elements in the given iterables
+    """
+    zips = list(zip_longest(*iterables))  # zip the iterables
+    return [item for tup in zips
+            for item in tup if item is not None]
+
+
+def interleave_generator(*iterables):
+    """
+    Interleave the given iterables - generator implementation
+    :param iterables: the iterables to interleave
+    :yield: an item of the interleaved list
+    """
+    for tup in zip_longest(*iterables):
+        for item in tup:
+            if item is not None:
+                yield item
+
+
+if __name__ == '__main__':
+    # regular version
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+    # generator version
+    print(list(interleave_generator('abc', [1, 2, 3], ('!', '@', '#'))))
